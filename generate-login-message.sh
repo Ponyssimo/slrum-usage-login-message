bf431eaffdbf5acdae4c6db88a36c227e6bc858e
Jeffrey Condell <jdcrc@rit.edu>
2024-08-26 09:47:46 -0400

    removed testing echoes and added usage summary

--- generate-login-message.sh
+++ generate-login-message.sh
@@ -10,23 +10,19 @@ maxJob=0
 maxMem=0
 minJob=0
 minMem=0
-currJob=""
+currJob=0
 currMem=0
 currReq=0
 accum=0
 count=0
 
 while read -r line; do
-    echo "line: $line"
     info=$(echo $line | cut -d '|' -f 2)
-    echo "$info"
     if [ "$info" == "TRESUsageInTot" ]; then
-        echo "skipping"
         continue
     fi
     if [  "$info" != "" ]; then
         usage=$(echo $info | cut -d ',' -f 4 | tr -d -c 0-9)
-        #need to check within current job for max here
         if [ $usage > $currMem ]; then
             currMem=$usage
         fi
@@ -37,13 +33,12 @@ while read -r line; do
                 maxJob=$currJob
                 maxMem=$percent
             fi
-            if [ "$percent" < "$minMem" ] || [ minJob == 0 ]; then
+            if [ "$percent" < "$minMem" ] || [ minMem == 0 ]; then
                 minJob=$currJob
                 minMem=$percent
             fi
             ((accum+=$percent))
         fi
-        #need to check previous job for max and min here
         currJob=$(echo $line | cut -d '|' -f 1)
         req=$(echo $line | cut -d '|' -f 3)
         if [ "$(echo "$req" | tr -d 0-9)" == "K" ]; then
@@ -56,15 +51,11 @@ while read -r line; do
             ((currReq=1073741824 * $(echo "$req" | tr -d -c 0-9)))
         fi
         ((count++))
-        #should probably reset job specific variables
         currMem=0
     fi
-    echo "$count"
-    echo -e "done\n"
 done < <(sacct --starttime=now-2weeks --endtime=now --state=COMPLETED -u $user --format jobid,TRESUsageInTot,ReqMem -p)
-
-echo "$accum / $count"
 
 avg=$((accum / count))
 
-echo "$avg%"
+echo "Your worst performing recent job was job number $minJob with $minMem% memory usage"
+echo "Your average recent memory usage was $avg%"
