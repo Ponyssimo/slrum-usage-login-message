4bc3b5416c37eb8c7e5838f84f94b977bae13dc8
Jeffrey Condell <jdcrc@rit.edu>
2024-10-04 10:26:38 -0400

    changed to write each user's message to an individual file

--- generate-message-chron.sh
+++ generate-message-chron.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-dir=$(dirname "$0")
-filedir="$dir/messages.txt"
-
-if [ -f $filedir ]; then
-    rm $filedir
-fi
-touch $filedir
-
 while read -r line; do
     currUser=$(echo $line | cut -d '|' -f 1)
     if [ $currUser == User ]; then
         continue
     fi
-    echo "$currUser" >> $filedir
-    ("$dir/generate-login-message.sh" $currUser) >> $filedir
+    filedir="/var/spool/motd/$currUser"
+
+    if [ -f $filedir ]; then
+        rm $filedir
+    fi
+
+    message=$("$dir/generate-login-message.sh" $currUser)
+    
+    if [ $message != "" ]; then
+        touch $filedir
+        echo "$message" > $filedir
+    fi
 done< <(sacctmgr show user -P)
