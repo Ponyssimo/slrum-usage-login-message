7f8add9854cd7d9d4cee6627b50d1b62a1a66ed7
Jeffrey Condell <jdcrc@rit.edu>
2024-10-16 10:44:09 -0400

    added comments and process_job function

--- generate-login-message.sh
+++ generate-login-message.sh
@@ -6,6 +6,7 @@ if [ ! -v $1 ]; then
     user=$1
 fi
 
+memThreshold=2097152
 maxJob=0
 maxMem=0
 minJob=0
@@ -16,30 +17,44 @@ currReq=0
 accum=0
 count=0
 
+# If the previous loop was a job, calculate the percent of the allocated memory used,
+# then determine whether it was the most or least efficiently allocated job
+function process_job {
+    if [ $currJob != 0 ]; then
+        percent=$(awk "BEGIN { pc=100*${currMem}/${currReq}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
+        if [ $percent -gt $maxMem ]; then
+            maxJob=$currJob
+            maxMem=$percent
+        fi
+        if [ $percent -lt $minMem ] || [ $minMem -eq 0 ]; then
+            minJob=$currJob
+            minMem=$percent
+        fi
+        ((accum+=$percent))
+    fi
+}
+
 while read -r line; do
+    # Get next line, skipping the first line
     info=$(echo $line | cut -d '|' -f 2)
     if [ "$info" == "TRESUsageInTot" ]; then
         continue
     fi
+
+    # If $line gives usage info and the requested memory is less than the allocated threshold,
+    # add the memory usage to the accumulator
     if [  "$info" != "" ]; then
-        if [ $currReq -lt 2097152 ]; then
+        if [ $currReq -lt $memThreshold ]; then
             continue
         fi
         usage=$(echo $info | cut -d ',' -f 4 | tr -d -c 0-9)
         ((currMem+=$usage))
+        
+    # Else, process the previous job and reset for the new job
     else
-        if [ $currJob != 0 ]; then
-            percent=$(awk "BEGIN { pc=100*${currMem}/${currReq}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
-            if [ $percent -gt $maxMem ]; then
-                maxJob=$currJob
-                maxMem=$percent
-            fi
-            if [ $percent -lt $minMem ] || [ $minMem -eq 0 ]; then
-                minJob=$currJob
-                minMem=$percent
-            fi
-            ((accum+=$percent))
-        fi
+        process_job
+
+        # Set the current job id and the amount of memory requested in KiB
         currJob=$(echo $line | cut -d '|' -f 1)
         req=$(echo $line | cut -d '|' -f 3)
         if [ "$(echo "$req" | tr -d 0-9)" == "K" ]; then
@@ -51,6 +66,9 @@ while read -r line; do
         elif [ "$(echo "$req" | tr -d 0-9)" == "T" ]; then
             ((currReq=1073741824 * $(echo "$req" | tr -d -c 0-9)))
         fi
+
+        # If the number of cpus requested or the time elapsed are below the threshold,
+        # set the requested memory to 0 so they will be skipped
         reqCPU=$(echo $line | cut -d '|' -f 4)
         if [ $reqCPU -eq 1 ]; then
             curreq=0
@@ -60,29 +78,23 @@ while read -r line; do
         startSec=$(date -d $startTime +"%s")
         endSec=$(date -d $endTime +"%s")
         elapsedTime=$((endSec - startSec))
-        if [ elapsedTime -lt 180 ]; then
+        if [ $elapsedTime -lt 180 ]; then
             curreq=0
         fi
-        if [ $currReq -gt 2097152 ]; then
+
+        # If the requested memory is at or above the threshold, increment the job count
+        if [ $currReq -ge $memThreshold ]; then
             ((count++))
         fi
+
+        # Reset the memory used accumulator
         currMem=0
     fi
 done < <(sacct --starttime=now-2weeks --endtime=now --state=COMPLETED -u $user --format jobid,TRESUsageInTot,ReqMem,ReqCPUS,Start,End -p)
 
-if [ $currJob != 0 ]; then
-    percent=$(awk "BEGIN { pc=100*${currMem}/${currReq}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
-    if [ $percent -gt $maxMem ]; then
-        maxJob=$currJob
-        maxMem=$percent
-    fi
-    if [ $percent -lt $minMem ] || [ $minMem -eq 0 ]; then
-        minJob=$currJob
-        minMem=$percent
-    fi
-    ((accum+=$percent))
-fi
+process_job
 
+# If at least one job was processed, calculate the average memory usage, then print the usage message
 if [ $count -ne 0 ]; then
     avg=$((accum / count))
 
