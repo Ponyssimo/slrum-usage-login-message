4913573fc10ae29176fd4f173bb69a22782a92bb
Jeffrey Condell <jdcrc@rit.edu>
2024-10-18 10:41:41 -0400

    fixed path to generate-login-message in generate-message-chron

--- generate-message-chron.sh
+++ generate-message-chron.sh
@@ -15,7 +15,7 @@ while read -r line; do
     fi
 
     # Generate the message for the current user
-    message=$("$dir/generate-login-message.sh" $currUser)
+    message=$("$(dirname "$0")/generate-login-message.sh" $currUser)
     
     # Create a new file containing the login message
     # if the message isn't empty
